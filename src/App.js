e3869431b960ffab462cc9b0cd9ddbe8a7320ae8
Goodness Ezeokafor <gootech442@gmail.com>
2021-08-12 13:17:38 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import MyAppleSigninButton from "./SigninButtton";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        <MyAppleSigninButton />
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
